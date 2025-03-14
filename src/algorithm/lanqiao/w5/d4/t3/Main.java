3f1272b9ec0fe1f4637090beedbb10df11c2d869
Zephyrtoria <2594379136@qq.com>
2025-03-14 16:09:49 +0800

    货币系统

---
+++ src/algorithm/lanqiao/w5/d4/t3/Main.java
@@ -0,0 +1,28 @@
+package algorithm.lanqiao.w5.d4.t3;
+
+import java.util.*;
+
+public class Main {
+    static final int N = 10010;
+    static long[] f = new long[N];
+    static int[] v = new int[30];
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int V = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            v[i] = sc.nextInt();
+        }
+
+        // dp
+        // 初始化
+        f[0] = 1;
+        for (int i = 1; i <= n ; i++) {
+            for (int j = v[i]; j <= V; j++) {
+                f[j] += f[j - v[i]];
+            }
+        }
+        System.out.println(f[V]);
+    }
+}
