3e70fae6e4a891169f87486389eabff52ee70c0f
Zephyrtoria <2594379136@qq.com>
2025-04-01 13:35:09 +0800

    孤独的照片

--- src/algorithm/lanqiao/w2/d5/t1/Main.java
+++ src/algorithm/lanqiao/w2/d5/t1/Main.java
@@ -1,49 +1,44 @@
-/**
- * @Author: Zephyrtoria
- * @CreateTime: 2025-02-24
- * @Description:
- * @Version: 1.0
- */
-
 package algorithm.lanqiao.w2.d5.t1;
 
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
-    static int N = 500010;
-    static String cows;
-    static int[] l = new int[N];
-    static int[] r = new int[N];
+    static final int N = 500010;
+    static char[] cow;
+    static int[] ls = new int[N], rs = new int[N];
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        cows = sc.next();
+        cow = sc.next().toCharArray();
 
         // 初始化连续的H和G的长度数组
-        // l[i]表示在i左侧有多少连续个与i不同的牛；r[i]表示在i右侧有多少连续个与i不同的牛
-        int sh = 0, sg = 0;
-        for (int i = 0; i < n; i++) {
-            if (cows.charAt(i) == 'H') {
-                l[i] = sg;
-                sg = 0;
-                sh++;
+        // ls[i]表示在i左侧有多少连续个与i不同的牛；rs[i]表示在i右侧有多少连续个与i不同的牛
+        char preCow = cow[0];
+        ls[0] = 0;
+        int count = 1;
+        for (int i = 1; i < n; i++) {
+            if (cow[i] == preCow) {
+                ls[i] = 0;
+                count++;
             } else {
-                l[i] = sh;
-                sh = 0;
-                sg++;
+                ls[i] = count;
+                count = 1;
+                preCow = cow[i];
             }
         }
 
-        sg = sh = 0;
-        for (int i = n - 1; i >= 0; i--) {
-            if (cows.charAt(i) == 'H') {
-                r[i] = sg;
-                sg = 0;
-                sh++;
+        preCow = cow[n - 1];
+        rs[n - 1] = 0;
+        count = 1;
+        for (int i = n - 2; i >= 0; i--) {
+            if (cow[i] == preCow) {
+                rs[i] = 0;
+                count++;
             } else {
-                r[i] = sh;
-                sh = 0;
-                sg++;
+                rs[i] = count;
+                count = 1;
+                preCow = cow[i];
             }
         }
 
@@ -51,11 +46,17 @@ public class Main {
         // 将判断区间的操作转换为判断牛的操作
         long res = 0;
         for (int i = 0; i < n; i++) {
-            // 三种情况实际上同时只会出现一种，所以可以直接写在一起
-            // 同时max()保证不会出现负数
-            res += (long)l[i] * r[i] + Math.max(r[i] - 1, 0) + Math.max(l[i] - 1, 0);
+            // 全取左，全取右，左右都有取
+            if (ls[i] >= 2) {
+                res += ls[i] - 1;
+            }
+            if (rs[i] >= 2) {
+                res += rs[i] - 1;
+            }
+            if (ls[i] + rs[i] >= 2) {
+                res += (long) ls[i] * rs[i];
+            }
         }
         System.out.println(res);
     }
-
 }
