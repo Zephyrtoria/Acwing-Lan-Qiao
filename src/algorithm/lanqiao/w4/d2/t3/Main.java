aaca321093df8f8efb8443190bbb2ccf5800b4ea
Zephyrtoria <2594379136@qq.com>
2025-04-03 08:36:40 +0800

    走迷宫

---
+++ src/algorithm/lanqiao/w4/d2/t3/Main.java
@@ -0,0 +1,49 @@
+package algorithm.lanqiao.w4.d2.t3;
+
+import java.util.*;
+
+public class Main {
+    static final int N = 110;
+    static int[][] graph = new int[N][N];
+    static boolean[][] visited = new boolean[N][N];
+    static int n, m;
+    static final int[] fx = new int[] {0, 0, 1, -1};
+    static final int[] fy = new int[] {1, -1, 0, 0};
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                graph[i][j] = sc.nextInt();
+            }
+        }
+
+        int res = bfs();
+        System.out.println(res);
+    }
+
+    private static int bfs() {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{1, 1, 0});
+        visited[1][1] = true;
+
+        while (!queue.isEmpty()) {
+            int[] cur = queue.remove();
+            if (cur[0] == n && cur[1] == m) {
+                return cur[2];
+            }
+
+            for (int i = 0; i < 4; i++) {
+                int dx = cur[0] + fx[i];
+                int dy = cur[1] + fy[i];
+                if (1 <= dx && dx <= n && 1 <= dy && dy <= m && !visited[dx][dy] && graph[dx][dy] == 0) {
+                    visited[dx][dy] = true;
+                    queue.add(new int[]{dx, dy, cur[2] + 1});
+                }
+            }
+        }
+        return -1;
+    }
+}
